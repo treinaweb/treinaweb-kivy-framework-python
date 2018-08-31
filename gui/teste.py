4aa075497bdddad9392280967e69b83ab6f190c1
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-08-31 16:08:10 -0300

    03 - Primeiros contatos com a linguagem kv

--- gui/teste.py
+++ gui/teste.py
@@ -1,10 +1,17 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 
+
+class Principal(Widget):
+    pass
+
+class Secundario(Widget):
+    pass
 
 class Teste(App):
     def build(self):
-        return Label(text='Olá, Mundo!!', font_size=40)
+        return Secundario()
 
 Teste().run()
