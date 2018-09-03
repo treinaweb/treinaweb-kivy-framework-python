94348523d0c885da39a1a048d57ed180b82bbd15
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-09-03 14:38:09 -0300

    04 - Trabalhando com eventos

--- gui/teste.py
+++ gui/teste.py
@@ -1,17 +1,19 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
 
-class Principal(Widget):
-    pass
+class Principal(BoxLayout):
+    def teste(self):
+        print("O método foi chamado")
 
 class Secundario(Widget):
     pass
 
 class Teste(App):
     def build(self):
-        return Secundario()
+        return Principal()
 
 Teste().run()
