a827e26278b206b5e7fb142fcaed63ddb3968de6
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-09-03 14:57:54 -0300

    05 - Propriedades Reativas

--- gui/teste.py
+++ gui/teste.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
@@ -6,8 +7,11 @@ from kivy.uix.widget import Widget
 
 
 class Principal(BoxLayout):
+    texto_principal = StringProperty('Eu sou uma label')
+    tamanho_texto_principal = NumericProperty(30)
     def teste(self):
-        print("O método foi chamado")
+        self.texto_principal = 'Fui clicado'
+        self.tamanho_texto_principal += 20
 
 class Secundario(Widget):
     pass
