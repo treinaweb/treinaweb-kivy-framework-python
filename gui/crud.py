894b1c0b78d44448194e3debc5a0c35a7f49162b
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-09-09 22:41:44 -0300

    12 - Editando clientes

--- gui/crud.py
+++ gui/crud.py
@@ -14,13 +14,29 @@ class BotaoListagem(ToggleButton):
         self.text = self.nome_cliente + " " + self.idade_cliente
         self.group = 'clientes'
 
+    def _do_release(self, *args):
+        Principal().cliente_selecionado(self.id_cliente)
 
 class Principal(BoxLayout):
+    id_cliente = 0
 
     def __init__(self, **kwargs):
         super(Principal, self).__init__(**kwargs)
         self.listar_clientes()
 
+    def cliente_selecionado(self, id):
+        Principal.id_cliente = id
+        #print(id)
+
+    def editar_cliente(self):
+        id = Principal.id_cliente
+
+        nome = self.ids.nome.text
+        idade = self.ids.idade.text
+        cli = cliente.Cliente(nome, idade)
+
+        cliente_repositorio.ClienteRepositorio.editar_cliente(id, cli)
+        self.listar_clientes()
 
     def cadastrar_cliente(self):
         nome = self.ids.nome.text
